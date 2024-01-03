2fb894dd32ca76501778885c62e4f210d6707e8f
DOMINICKK1 <16833224-DOMINICKK1@users.noreply.replit.com>
2024-01-03 13:39:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Shantanu Saha");
+    System.out.println("Panera Charged Lemonade");
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
